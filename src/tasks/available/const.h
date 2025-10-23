e3423753efdb7de2fb6a39b5a9b5dd72ec35d92d
htchan <jacky19980619@gmail.com>
2025-10-23 19:15:27 +0800

    chore: update loop interval and expire available after 90s

--- src/tasks/available/const.h
+++ src/tasks/available/const.h
@@ -22,6 +22,7 @@ const std::string AVAILABLE_DISCOVERY_PAYLOAD =
     AVAILABLE_STATE_PAYLOAD_OFFLINE + R"(",
     "availability_topic":")" +
     AVAILABLE_ENTITY + STATE_TOPIC + R"(",
+    "expire_after":90,
     "entity_category":"diagnostic",
     "device_class":"connectivity"
 })";
